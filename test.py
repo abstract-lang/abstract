3fc01545be7b11c469f590c48b9caa15594c7039
itttgg <spdd5724@gmail.com>
2022-08-30 17:53:56 +0300

    Add many tests!

--- test.py
+++ test.py
@@ -11,6 +11,28 @@ class TokenizeTests(unittest.TestCase):
     
     def test_int(self):
         self.assertEqual(lex('1234'), [('1234', 'INT')])
+    
+    def test_float(self):
+        self.assertEqual(lex('12.34'), [('12.34', 'FLOAT')])
+    
+    def test_bool(self):
+        self.assertEqual(lex('true'), [('true', 'BOOLEAN')])
+        self.assertEqual(lex('false'), [('false', 'BOOLEAN')])
+    
+    def test_print(self):
+        self.assertEqual(lex('print()'), [('print', 'FUNCTION'), ('(', 'RESERVED'), (')', 'RESERVED')])
+    
+    def test_id(self):
+        self.assertEqual(lex('abc'), [('abc', 'ID')])
+    
+    def test_types(self):
+        self.assertEqual(lex('str'), [('str', 'TYPE')])
+        self.assertEqual(lex('int'), [('int', 'TYPE')])
+        self.assertEqual(lex('flo'), [('flo', 'TYPE')])
+        self.assertEqual(lex('bool'), [('bool', 'TYPE')])
+
+    def test_var(self):
+        self.assertEqual(lex('int abc = 34'), [('int', 'TYPE'), ('abc', 'ID'), ('=', 'RESERVED'), ('34', 'INT')])
 
 if __name__ == "__main__":
     unittest.main()
